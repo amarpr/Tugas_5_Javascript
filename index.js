5ced2593c571b0e1dccc62551e08c557dc2512be
Muhamar <amararrasyid@gmail.com>
2020-05-03 12:55:38 +0700

    Tugas 5 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,12 @@
+// Menampilkan perulangan sebanyak 20
+for (var i = 1; i <= 20; i++) {
+    console.log("ini perulangan ke " + i)
+}
+
+console.log("===============================")
+
+// Menampilkan Kelipatan 3
+for (var i = 1; i <= 20; i++) {
+    if (i % 3 == 0)
+        console.log(i)
+}
